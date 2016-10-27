b137b21f257770363e732503a9842fecd9a901e9
Andy <hiandyzhang@gmail.com>
2016-10-27 22:20:17 +0000

    generate basic controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'user/index'
+
+  get 'user/show'
+
+  get 'user/new'
+
+  get 'user/create'
+
+  get 'user/edit'
+
+  get 'user/update'
+
+  get 'user/delete'
+
+  get 'user/destroy'
+
   get 'geomap/index'
   get 'geomap/directions'
   root 'geomap#index'
