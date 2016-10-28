834ee8b9d32cc84af8b9c546f4bb41cf90a99803
Andy <hiandyzhang@gmail.com>
2016-10-28 04:55:35 +0000

    add test case for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,26 @@
-class User < ApplicationRecord
-    #has_many :restaurants, dependent: :destroy 
-    #accepts_nested_attributes_for :tokimons
-    has_secure_password
+# class User < ApplicationRecord
+#     #has_many :restaurants, dependent: :destroy 
+#     #accepts_nested_attributes_for :tokimons
+   
     
-    before_save { self.email = email.downcase }
-    validates :name, presence: true, length: { maximum: 30 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-	validates :email, presence: true, length: { maximum: 200 }, 
-	            format: { with: VALID_EMAIL_REGEX },
-				uniqueness: { case_sensitive: false}
-	#after_initialize :init_config		
-end
+#     before_save { self.email = email.downcase }
+#     validates :name, presence: true, length: { maximum: 30 }
+# 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+# 	validates :email, presence: true, length: { maximum: 200 }, 
+# 	                  format: { with: VALID_EMAIL_REGEX },
+# 				uniqueness: { case_sensitive: false}
+# 	has_secure_password
+# 	validates :password, presence: true, length: { minimum: 6 }
+# end
 
+class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-    def setup
+  def setup
     @user = User.new(name: "Example User", email: "user@example.com")
   end
 
@@ -41,16 +41,26 @@ class UserTest < ActiveSupport::TestCase
     end
   end
   
-  test "email addresses should be unique" do
-    duplicate_user = @user.dup
-    @user.save
-    assert_not duplicate_user.valid?
-  end
+  # test "email addresses should be unique" do
+  #   duplicate_user = @user.dup
+  #   @user.save
+  #   assert_not duplicate_user.valid?
+  # end
   
-  test "email addresses should be saved as lower-case" do
-    mixed_case_email = "Foo@ExAMPle.CoM"
-    @user.email = mixed_case_email
-    @user.save
-    assert_equal mixed_case_email.downcase, @user.reload.email
+  # test "email addresses should be saved as lower-case" do
+  #   mixed_case_email = "Foo@ExAMPle.CoM"
+  #   @user.email = mixed_case_email
+  #   @user.save
+  #   assert_equal mixed_case_email.downcase, @user.reload.email
+  # end
+  
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
