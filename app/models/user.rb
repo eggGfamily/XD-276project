31041d651f5ece12224c3781a23a6f09674da7bc
Andy <hiandyzhang@gmail.com>
2016-10-28 04:23:13 +0000

    migrate name and email attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
     #has_many :restaurants, dependent: :destroy 
     #accepts_nested_attributes_for :tokimons
-    
-    validates :name, presence: true, length: { maximum: 20 }
+    before_save { self.email = email.downcase }
+    validates :name, presence: true, length: { maximum: 30 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
+	validates :email, presence: true, length: { maximum: 200 }, 
+	            format: { with: VALID_EMAIL_REGEX },
 				uniqueness: { case_sensitive: false}
 	#after_initialize :init_config		
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema.define(version: 20161027222157) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
