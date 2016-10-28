aafc0bab8684c6ba6006dc54a6de999d4a8c912f
Andy <hiandyzhang@gmail.com>
2016-10-28 03:04:19 +0000

    start accountsystem branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    #has_many :restaurants, dependent: :destroy 
+    #accepts_nested_attributes_for :tokimons
+    
+    validates :name, presence: true, length: { maximum: 20 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
+				uniqueness: { case_sensitive: false}
+	#after_initialize :init_config		
 end
+
+
