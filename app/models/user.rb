74dea4d20f8bdf4b5435fb5f4c0b5c9f27c9e2a9
qinxinx <qinxinx@sfu.ca>
2016-11-03 16:11:16 -0700

    login test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 # class User < ApplicationRecord
-#     #has_many :restaurants, dependent: :destroy 
+#     #has_many :restaurants, dependent: :destroy
 #     #accepts_nested_attributes_for :tokimons
-   
-    
+
+
 #     before_save { self.email = email.downcase }
 #     validates :name, presence: true, length: { maximum: 30 }
 # 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-# 	validates :email, presence: true, length: { maximum: 200 }, 
+# 	validates :email, presence: true, length: { maximum: 200 },
 # 	                  format: { with: VALID_EMAIL_REGEX },
 # 				uniqueness: { case_sensitive: false}
 # 	has_secure_password
@@ -22,5 +22,11 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-end
 
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+end
